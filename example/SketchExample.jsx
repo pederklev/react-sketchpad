fad487429f39ca5e25c0ea1fb8920da8263d258d
pederklev <pederklev@gmail.com>
2017-04-22 18:03:24 +0200

    remove tool choices from interface

--- example/SketchExample.jsx
+++ example/SketchExample.jsx
@@ -23,6 +23,7 @@ export default class SketchExample extends Component
 
   render() {
     const { tool, size, color, fill, fillColor, items } = this.state;
+    console.log(this.state);
     return (
       <div>
         <h1>React SketchPad</h1>
@@ -38,48 +39,7 @@ export default class SketchExample extends Component
             tool={tool}
           />
         </div>
-        <div style={{float:'left'}}>
-          <div className="tools" style={{marginBottom:20}}>
-            <button
-              style={tool == TOOL_PENCIL ? {fontWeight:'bold'} : undefined}
-              className={tool == TOOL_PENCIL  ? 'item-active' : 'item'}
-              onClick={() => this.setState({tool:TOOL_PENCIL})}
-            >Pencil</button>
-            <button
-              style={tool == TOOL_LINE ? {fontWeight:'bold'} : undefined}
-              className={tool == TOOL_LINE  ? 'item-active' : 'item'}
-              onClick={() => this.setState({tool:TOOL_LINE})}
-            >Line</button>
-            <button
-              style={tool == TOOL_ELLIPSE ? {fontWeight:'bold'} : undefined}
-              className={tool == TOOL_ELLIPSE  ? 'item-active' : 'item'}
-              onClick={() => this.setState({tool:TOOL_ELLIPSE})}
-            >Ellipse</button>
-            <button
-              style={tool == TOOL_RECTANGLE ? {fontWeight:'bold'} : undefined}
-              className={tool == TOOL_RECTANGLE  ? 'item-active' : 'item'}
-              onClick={() => this.setState({tool:TOOL_RECTANGLE})}
-            >Rectangle</button>
-          </div>
-          <div className="options" style={{marginBottom:20}}>
-            <label htmlFor="">size: </label>
-            <input min="1" max="20" type="range" value={size} onChange={(e) => this.setState({size: parseInt(e.target.value)})} />
-          </div>
-          <div className="options" style={{marginBottom:20}}>
-            <label htmlFor="">color: </label>
-            <input type="color" value={color} onChange={(e) => this.setState({color: e.target.value})} />
-          </div>
-          {(this.state.tool == TOOL_ELLIPSE || this.state.tool == TOOL_RECTANGLE) ?
-            <div>
-              <label htmlFor="">fill in:</label>
-              <input type="checkbox" value={fill} style={{margin:'0 8'}}
-                     onChange={(e) => this.setState({fill: e.target.checked})} />
-              {fill ? <span>
-                  <label htmlFor="">with color:</label>
-                  <input type="color" value={fillColor} onChange={(e) => this.setState({fillColor: e.target.value})} />
-                </span> : ''}
-            </div> : ''}
-        </div>
+
       </div>
     );
   }
