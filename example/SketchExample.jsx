c3a07eadb7a9a492794d3a6f20bae916b484c0b5
pederklev <pederklev@gmail.com>
2017-04-22 18:07:18 +0200

    stripping more stuff

--- example/SketchExample.jsx
+++ example/SketchExample.jsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import { SketchPad, TOOL_PENCIL, TOOL_LINE, TOOL_RECTANGLE, TOOL_ELLIPSE } from './../src';
 
-
-
 export default class SketchExample extends Component
 {
-  socket = null;
 
   constructor(props) {
     super(props);
@@ -16,13 +13,10 @@ export default class SketchExample extends Component
     }
   }
 
-
   render() {
-    const { tool, size, color, fill, fillColor, items } = this.state;
-    console.log(this.state);
+    const { size, items } = this.state;
     return (
       <div>
-        <h1>React SketchPad</h1>
         <div style={{float:'left', marginRight:20}}>
           <SketchPad
             size={size}
